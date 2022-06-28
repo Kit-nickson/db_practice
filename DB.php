b682997bbd2fde4c882bde957b63fffedf14e740
Kit-nickson <niknikant@rambler.ru>
2022-06-29 00:06:58 +0300

    added update(), addColumn(), deleteColumn() functions

--- DB.php
+++ DB.php
@@ -5,10 +5,13 @@ declare(strict_types = 1);
 class DB 
 {
     protected PDO $db;
+    protected const DATABASE = '';
 
     
     public function __construct(string $dsn = 'mysql:host=localhost', string $username = 'root', string $password = 'root')
     {
+
+        // should check if the database is set
         try {
             $this->db = new PDO($dsn, $username, $password);
         } catch (PDOException $e) {
@@ -27,6 +30,13 @@ class DB
     public function __call($name, $arguments)
     {
         print $name.' method does not exsit...';
+    }
+
+
+    // should check if databse is set
+
+    public function isDBSet () {
+       
     }
 
 
@@ -71,6 +81,21 @@ class DB
     public function deleteTable(string $db, string $table): void
     {
         $this->db->exec("USE $db; DROP TABLE $table");
+    }
+
+    // update table
+
+    public function addColumn(string $db, string $table, string $col): void
+    {
+        $this->db->exec("USE $db;");
+        $this->db->exec("ALTER TABLE $table ADD $col");
+    }
+
+
+    public function deleteColumn(string $db, string $table, string $col): void
+    {
+        $this->db->exec("USE $db;");
+        $this->db->exec("ALTER TABLE $table DROP $col");
     }
 
 
@@ -127,6 +152,33 @@ class DB
 
 
         $this->db->exec("USE $db; INSERT INTO `$table` ($colsStr) VALUES ($valuesStr)");
+    }
+
+    // update
+
+    public function update(
+        string $db,
+        string $table,
+        array $columns,
+        array $values,
+        string $condition
+    ): void
+    {
+        if (count($columns) !== count($values)) {
+            throw new RuntimeException('Columns and values should contain the same amount of items');
+        }
+
+        $colvalues = '';
+
+        foreach ($columns as $key => $column) {
+            $colvalues .= "$column = '$values[$key]', ";
+        }
+
+        $colvalues = trim($colvalues, ', ');
+
+        print($colvalues);
+
+        $this->db->exec("USE $db; UPDATE $table SET $colvalues WHERE $condition");
     }
 
 
